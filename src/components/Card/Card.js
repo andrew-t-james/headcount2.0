58084d9be8929678cb99efd9c0dabcd1c70480c1
Andrew James <ajames20@gmail.com>
2018-07-01 15:20:40 -0600

    Add test to cover clicking on card to get to 100% on all tests

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -16,7 +16,7 @@ const Card = ({ location, stats, selected, handleSelectedDistrict }) => {
   return (
     <div onClick={() => handleSelectedDistrict(location)} className={`card ${selected ? 'selected' : ''}`}>
       <h2 className="card-location">{location}</h2>
-      <ul className={selected ? 'card-list' : 'card-list collapsed-list'}>
+      <ul className={selected ? 'card-list' : 'collapsed-list'}>
         {districtStats}
       </ul>
     </div>
