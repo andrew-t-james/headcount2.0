98478bd40274ab56e3a53ab53ea644993806e0c0
Andrew James <ajames20@gmail.com>
2018-07-01 17:25:22 -0600

    Add defualt message to ComparisonCard

--- src/components/ComparisonCard/ComparisonCard.js
+++ src/components/ComparisonCard/ComparisonCard.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './ComparisonCard.css';
+
 const ComparisonCard = props => {
   const comparedDistrictKeys = Object.keys(props);
-
+  if (!comparedDistrictKeys.length) {
+    return (
+      <div>
+        <h2 className="comparison-card-message">Please Add a district</h2>
+      </div>
+    );
+  }
   return (
+
     <div>
       { comparedDistrictKeys[0] &&
         <h2>{comparedDistrictKeys[0]} : {props[comparedDistrictKeys[0]]}</h2>
